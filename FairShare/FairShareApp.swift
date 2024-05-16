bfdba8b91a7ec4c6cf388100e44d2c7951adae5a
SankrityaT <sthaku20@asu.edu>
2024-05-16 13:50:38 -0700

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Sankritya Thakur on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
